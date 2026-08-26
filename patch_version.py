d0c0fb3344c4c78618f683133d5a2431fa43e7cd
Charles Michael Cayno <Charles.Cayno@power.com>
2026-08-26 18:20:40 +0800

    chore: bump version to v4 to bypass cache

--- patch_version.py
+++ patch_version.py
@@ -1,13 +1,17 @@
 import re
-for filename in ['index.html', 'index_firebase.html']:
-    with open(filename, 'r', encoding='utf-8') as f:
-        html = f.read()
-    html = re.sub(r'Resource Inventory', 'Resource Inventory v3', html, count=1)
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.write(html)
+
+def process(filename, encoding):
+    with open(filename, 'r', encoding=encoding) as f:
+        content = f.read()
+    content = re.sub(r'Resource Inventory( v\d+)?', 'Resource Inventory v3', content, count=1)
+    with open(filename, 'w', encoding=encoding) as f:
+        f.write(content)
+
+process('index.html', 'utf-8')
+process('index_firebase.html', 'utf-16')
 
 with open('sw.js', 'r', encoding='utf-8') as f:
     sw = f.read()
-sw = re.sub(r'glc-inventory-v2', 'glc-inventory-v3', sw)
+sw = re.sub(r'glc-inventory-v2', 'glc-inventory-v4', sw)
 with open('sw.js', 'w', encoding='utf-8') as f:
     f.write(sw)
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'glc-inventory-v2';
+const CACHE_NAME = 'glc-inventory-v4';
 const PRECACHE_ASSETS = [
   './',
   './index.html',
